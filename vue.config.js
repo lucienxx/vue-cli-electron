61efa6e4a21a9c47216414e46126a5b57d155df3
lucienxx <lucienx@gmail.com>
2020-04-09 13:55:01 +0800

    引入jquery

--- vue.config.js
+++ vue.config.js
@@ -30,3 +30,16 @@ module.exports = {
     }
   }
 }
+const webpack = require('webpack')
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      // 支持 jquery
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+      })
+    ],
+  }
+}
